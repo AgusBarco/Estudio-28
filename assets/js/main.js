8b0c741fd5f522f5640f7048c3c9c0cc01288054
Agustin.Barco <agu.cba65@gmail.com>
2022-07-04 19:57:53 -0300

    CambiosJs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,62 @@
-for(let i = 1;i > 10 ; i++)
+/*for(let i = 1;i < 10 ; i++)
 {let nombre = prompt ("ingrese un nombre")
-console.log("Turno numero: " + i + "Nombre: "+ nombre)}
+console.log("Turno numero: " + i + "Nombre: "+ nombre)}*/
+
+
+let pregunta = parseInt(prompt('que productos quiere llevar?  1-algo 2-algo 3-algo 4-algo, digite 0 para salir'))
+let cantidad= 0
+let precioTotal= 0
+
+function suma(n) {
+    precioTotal+=n
+}
+while(pregunta != 0){
+    switch (pregunta) {
+        case 1:
+            let producto1 = 1500
+            cantidad+=1
+            suma(producto1)
+            break;
+        case 2:
+            let producto2 = 2000
+            cantidad+=1
+            suma(producto2)
+            break;
+        case 3:
+            let producto3 = 3000
+            cantidad+=1
+            suma(producto3)
+            break;
+        case 4:
+            let producto4= 4000
+            cantidad+=1
+            suma(producto4)
+            break;
+    
+        default:
+            alert('ese producto no esta en la lista')
+            break;
+    }
+
+    pregunta = parseInt(prompt('que productos quiere llevar?  1-algo 2-algo 3-algo 4-algo, digite 0 para salir'))
+}
+
+function descuento() {
+    if(cantidad == 2){
+      return  precioDescontado = precioTotal - (precioTotal * 0.20)
+    }else if(cantidad == 3){
+        return  precioDescontado = precioTotal - (precioTotal * 0.30)
+    }else{
+        return  precioTotal
+    }
+}
+
+alert('su total es de :$' + descuento())
+    
+
+    
+
+
+
+
+
